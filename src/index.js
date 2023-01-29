fe594e3d862d57dffd90d4c14284e8aa6af2f849
AL424 <algr5113@gmail.com>
2023-01-29 11:01:08 +0300

    feat: added function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result; 
+  let resultStr = str;
+  
+  let i = 0;
+
+  while (i < resultStr.length) {
+    let initialLengthStr = resultStr.length;
+
+    bracketsConfig.forEach(item => {
+      if (resultStr[i] === item[0] && resultStr[i + 1] === item[1]) resultStr = resultStr.slice(0, i) + resultStr.slice(i + 2);
+    })
+
+    if (initialLengthStr === resultStr.length) i += 1;
+    else i = 0;
+  }
+
+  if (resultStr === '') result = true;
+  else result = false;
+
+  return result
 }
